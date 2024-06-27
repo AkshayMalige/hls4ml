c9fdea122221aa76ac27102cc332e34b10c95e6c
akshaynevis <akshaym@nevis.columbia.edu>
2024-06-27 10:38:05 -0400

    tested on u250 vitis 32-stream

--- hls4ml/writer/vitis_writer.py
+++ hls4ml/writer/vitis_writer.py
@@ -30,5 +30,5 @@ class VitisWriter(VivadoWriter):
         """
         super().write_hls(model)
         self.write_nnet_utils_overrides(model)
-        os.remove(model.config.get_output_dir() + '.tar.gz')
+        # os.remove(model.config.get_output_dir() + '.tar.gz')
         self.write_tar(model)
